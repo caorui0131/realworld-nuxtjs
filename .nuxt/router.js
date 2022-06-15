90193fc9a72751903286818874dfbd2740494285
caorui0131 <280314737@qq.com>
2022-06-15 15:13:07 +0800

    导入剩余页面

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -7,6 +7,10 @@ import scrollBehavior from './router.scrollBehavior.js'
 const _0fe862ac = () => interopDefault(import('../pages/layout' /* webpackChunkName: "" */))
 const _361654e1 = () => interopDefault(import('../pages/home' /* webpackChunkName: "" */))
 const _e626e0b2 = () => interopDefault(import('../pages/login' /* webpackChunkName: "" */))
+const _45a3db32 = () => interopDefault(import('../pages/profile' /* webpackChunkName: "" */))
+const _59d44b25 = () => interopDefault(import('../pages/settings' /* webpackChunkName: "" */))
+const _041988ef = () => interopDefault(import('../pages/editor' /* webpackChunkName: "" */))
+const _780f5498 = () => interopDefault(import('../pages/article' /* webpackChunkName: "" */))
 
 const emptyFn = () => {}
 
@@ -34,6 +38,22 @@ export const routerOptions = {
       path: "/register",
       component: _e626e0b2,
       name: "register"
+    }, {
+      path: "/profile/:username",
+      component: _45a3db32,
+      name: "profile"
+    }, {
+      path: "/settings",
+      component: _59d44b25,
+      name: "settings"
+    }, {
+      path: "/editor",
+      component: _041988ef,
+      name: "editor"
+    }, {
+      path: "/article/:slug",
+      component: _780f5498,
+      name: "article"
     }]
   }],
 
